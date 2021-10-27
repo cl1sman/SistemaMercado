8a76635954795298647dbf00080fe4a7cdb31834
Matheus Clisman <34177582+cl1sman@users.noreply.github.com>
2021-10-27 13:48:34 -0400

    Create new repo, for use new issues

---
+++ src/Produto.java
@@ -0,0 +1,48 @@
+public class Produto {
+    // Atributos
+    private String produto;
+    private int quantidadeDeProdutos;
+    private float preço;
+    Produto[] vetorDeProdutos;
+
+
+    // Construct
+    public Produto(){
+
+    }
+    public Produto(int n){
+        vetorDeProdutos = new Produto[n]; // limite do vetor
+    }
+
+
+    // Métodos Personalizados
+    public void addProduto(String a, int b, float c){
+        this.setProduto(a);
+        this.setQuantidadeDeProdutos(b);
+        this.setPreço(c);
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidadeDeProdutos() {
+        return quantidadeDeProdutos;
+    }
+
+    public void setQuantidadeDeProdutos(int quantidadeDeProdutos) {
+        this.quantidadeDeProdutos = quantidadeDeProdutos;
+    }
+    public float getPreço() {
+        return preço;
+    }
+
+    public void setPreço(float preço) {
+        this.preço = preço;
+    }
+
+}
