0656854e49038297cec92632fa0f078cebb11e81
clisman <clisman@localhost.localdomain>
2021-10-28 01:33:42 -0400

    fiz um vetor que salva objetos

--- src/Carrinho.java
+++ src/Carrinho.java
@@ -5,14 +5,36 @@ import java.util.Scanner;
 class Carrinho{
     // Atributos
     Produto vetor[];
+    Produto item = new Produto();
 
+    public Carrinho() {
+    }
+    public Carrinho(int x){
+        setVetor(x);
+    }
+
+    // Métodos de acesso
     public void setVetor(int x) {
         this.vetor = new Produto[x];
     }
     
     void addProduto(){
         Scanner scan = new Scanner(System.in);
-        int continua = scan.nextInt();
-        // -[] colocar produtos dentro do vetor
+        for (int i = 0; i < vetor.length; i++) {
+            String nomeProduto = scan.next();
+            this.item.setProduto(nomeProduto);
+            int quantosProdutos = scan.nextInt();
+            this.item.setQuantidadeDeProdutos(quantosProdutos);
+            float preço = scan.nextFloat();
+            this.item.setPreço(preço);
+
+            vetor[i] = item;
+        }
+    }
+    float somarProdutos(Produto vet){
+        Produto vetorSoma = new Produto();
+        for (int i = 0; i < x; i++) {
+
+        }
     }
 }
--- src/SistemaMercado.java
+++ src/SistemaMercado.java
@@ -1,4 +1,8 @@
 package SistemaMercado;
 
 public class SistemaMercado {
+    public static void main(String[] args) {
+        Carrinho teste = new Carrinho(4);
+        teste.addProduto();
+    }
 }
